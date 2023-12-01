9352aec3b85f69799a3e1973dbf43e13021727bb
Anchaljajoo7 <anchal.jajoo7@gmail.com>
2023-12-01 19:05:32 +0530

    1/12/23/7.05pm

--- app/src/main/java/com/example/rideup/CarBookingSeater.java
+++ app/src/main/java/com/example/rideup/CarBookingSeater.java
@@ -1,14 +1,50 @@
 package com.example.rideup;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager.widget.ViewPager;
 
 import android.os.Bundle;
 
+import com.google.android.material.tabs.TabLayout;
+
 public class CarBookingSeater extends AppCompatActivity {
+    TabLayout t1;
+    ViewPager v1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_car_booking_seater);
+        initial();
     }
-}
+
+    private void initial() {
+        t1 = findViewById(R.id.tl_location);
+        v1 = findViewById(R.id.vp_seats);
+
+        t1.addTab(t1.newTab().setText("Select Seat"));
+        t1.addTab(t1.newTab().setText("Pickup Points"));
+        t1.addTab(t1.newTab().setText(" Drop Points"));
+        TabRideupAdapter1 myAdapter1 = new TabRideupAdapter1(CarBookingSeater.this, getSupportFragmentManager());
+        v1.setAdapter(myAdapter1);
+        v1.addOnPageChangeListener(new TabLayout.TabLayoutOnPageChangeListener(t1));
+        t1.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
+
+            @Override
+            public void onTabSelected(TabLayout.Tab tab) {
+
+            }
+
+            @Override
+            public void onTabUnselected(TabLayout.Tab tab) {
+
+            }
+
+            @Override
+            public void onTabReselected(TabLayout.Tab tab) {
+
+            }
+
+        });
+    }}
+
---
+++ app/src/main/java/com/example/rideup/TabRideupAdapter1.java
@@ -0,0 +1,37 @@
+package com.example.rideup;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentPagerAdapter;
+
+public class TabRideupAdapter1 extends FragmentPagerAdapter {
+    public TabRideupAdapter1( CarBookingSeater carBookingSeater,@NonNull FragmentManager fm) {
+        super(fm);
+    }
+
+    @NonNull
+    @Override
+    public Fragment getItem(int position) {
+
+        switch (position)
+        {
+            case 0:
+               SelectSeatTabLayout selectSeatTabLayout=new SelectSeatTabLayout();
+                return selectSeatTabLayout;
+            case 1:
+                PickupPointsTabLayout pickupPointsTabLayout=new PickupPointsTabLayout();
+                return pickupPointsTabLayout;
+            case 2:
+                DropPointsTabLayout dropPointsTabLayout=new DropPointsTabLayout();
+                return dropPointsTabLayout;
+
+        }
+        return null;
+    }
+
+    @Override
+    public int getCount() {
+        return 3;
+    }
+}
