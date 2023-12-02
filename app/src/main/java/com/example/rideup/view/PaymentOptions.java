a3e0524e70e807e883c2226c009e9f9827bbf205
Anchaljajoo7 <anchal.jajoo7@gmail.com>
2023-12-02 18:53:51 +0530

    2/12/23/6.53pm

--- app/src/main/java/com/example/rideup/view/PaymentOptions.java
+++ app/src/main/java/com/example/rideup/view/PaymentOptions.java
@@ -3,14 +3,37 @@ package com.example.rideup.view;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import com.example.rideup.R;
+import com.google.android.material.bottomsheet.BottomSheetDialog;
 
 public class PaymentOptions extends AppCompatActivity {
-
+Button button;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_payment_options);
+        initial();
+    }
+
+    private void initial() {
+        button=findViewById(R.id.b_proceedtopay);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showbottomseet();
+            }
+        });
+
+    }
+
+    private void showbottomseet() {
+        BottomSheetDialog bottomSheetDialog=new BottomSheetDialog(this);
+        bottomSheetDialog.setContentView(R.layout.bottomsheet_addnewcard_rideup);
+        bottomSheetDialog.show();
+
+
     }
 }
